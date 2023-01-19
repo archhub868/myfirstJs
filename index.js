97a66c12477ce8e4675234f194bdfb2e77387dda
Habakkuk Onchiri <73702963+archhub868@users.noreply.github.com>
2023-01-19 16:30:15 +0000

    subtraction and Area function in JS

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-//let i = 25;
-for (let i = 0; i <= 10; i++) {
-  if (i % 2 == 0); {
-
-    console.log(i);
-  }
-
+function subtract(a, b) {
+   
+    return  a - b;
+ 
 }
+ let a = prompt("Enter the value of a");
+ let b = prompt("Enter the value of b");
+console.log(subtract(a,b))
+
+//break;
+//calculating the area of a circle
+const getRectArea = function(width, length){
+  return width * length;
+}
+let width = prompt("Enter the width");
+let length = prompt("Enter the length"); 
+console.log(getRectArea(width,length));
